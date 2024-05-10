e52dde927b64448d5d71886d16d205a5c95cd588
IliePatriciaMaria <iliepatriciamaria012@gmail.com>
2024-05-10 16:04:31 +0300

    Second commit

--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function MainPage() {
   const [records, setRecords] = useState([]);
-
+ 
   useEffect(() => {
     fetchData();
   }, []);
